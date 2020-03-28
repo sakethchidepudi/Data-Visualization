b2a2b3b59752b441ee0eb24542422d292f6a5302
sakethchidepudi <54606035+sakethchidepudi@users.noreply.github.com>
2020-03-28 17:45:15 -0400

    Add files via upload

---
+++ Loading Data fromExcel.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+Location = "Downloads/AGS01.xls"
+df = pd.read_excel(Location)
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
---
+++ Loading data from CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+Location = "Downloads/all_040_in_01.P1.csv"
+df = pd.read_csv(Location, header=None)
+
+
+# In[2]:
+
+
+df.head()
+
+
+# In[3]:
+
+
+import pandas as pd
+Location = "Downloads/all_040_in_01.P1.csv"
+df = pd.read_csv(Location)
+
+
+# In[4]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
